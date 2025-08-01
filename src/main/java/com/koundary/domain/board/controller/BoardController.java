844595e0329e5da502d0d4d59561d610abfb5091
lionjr56 <lionjr56@naver.com>
2025-08-01 13:32:28 +0900

    borad 기능 추가

--- src/main/java/com/koundary/domain/board/controller/BoardController.java
+++ src/main/java/com/koundary/domain/board/controller/BoardController.java
@@ -1,4 +1,21 @@
 package com.koundary.domain.board.controller;
 
+import com.koundary.domain.board.dto.BoardResponse;
+import com.koundary.domain.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/boards")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping
+    public List<BoardResponse> getBoards() {
+        return boardService.getAllBoards();
+    }
 }
--- src/main/java/com/koundary/domain/board/repository/BoardRepository.java
+++ src/main/java/com/koundary/domain/board/repository/BoardRepository.java
@@ -1,4 +1,12 @@
 package com.koundary.domain.board.repository;
 
-public class BoardRepository {
+import com.koundary.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    Optional<Board> findByBoardCode(String boardCode);
+    List<Board> findAllByOrderByDisplayOrderAsc();
 }
